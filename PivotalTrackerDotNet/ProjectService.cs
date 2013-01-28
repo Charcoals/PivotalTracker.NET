bb9d78f982a15f444acde47addaf1b259f048f50
Tom Parker <tom.parker@spotlight.com>
2013-01-28 14:46:40 +0000

    If we see a JSON error, throw an exception with the relevant information in it

--- PivotalTrackerDotNet/ProjectService.cs
+++ PivotalTrackerDotNet/ProjectService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
 using PivotalTrackerDotNet.Domain;
 
 namespace PivotalTrackerDotNet {
@@ -37,6 +38,12 @@ namespace PivotalTrackerDotNet {
             var response = RestClient.Execute(request);
             var projects = new List<Project>();
             var serializer = new RestSharpXmlDeserializer();
+            if (response.Content.StartsWith("{")) {
+                var jObject = JObject.Parse(response.Content);
+                if (jObject.Property("error_message") != null) {
+                    throw new Exception(jObject.Property("error_message").Value.ToString());
+                }
+            }
             var el = XElement.Parse(response.Content);
             projects.AddRange(el.Elements("project").Select(project => serializer.Deserialize<Project>(project.ToString())));
             return projects;
