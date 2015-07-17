6fb8e47896dc047e10eb43d4231f6d72346f8f69
Martin Hyldahl <martin@hyldahlnet.dk>
2015-07-17 13:38:22 +0200

    Implement GetHashCode in Label

--- PivotalTrackerDotNet/Domain/Label.cs
+++ PivotalTrackerDotNet/Domain/Label.cs
@@ -2,10 +2,10 @@ namespace PivotalTrackerDotNet.Domain
 {
     public class Label
     {
-        public int? ProjectID { get; set; }
+        public int? ProjectId { get; set; }
         public string Name { get; set; }
         public string Kind { get; set; }
-        public int? ID { get; set; }
+        public int? Id { get; set; }
 
         public static implicit operator Label(string name)
         {
@@ -16,16 +16,29 @@ namespace PivotalTrackerDotNet.Domain
         {
             var other = obj as Label;
 
-            if (this.ProjectID != null && other.ProjectID != null && this.ProjectID != other.ProjectID)
+            if (this.ProjectId != null && other.ProjectId != null && this.ProjectId != other.ProjectId)
                 return false;
 
-            if (this.ID != null && other.ID != null && this.ID != other.ID)
+            if (this.Id != null && other.Id != null && this.Id != other.Id)
                 return false;
 
             if (this.Kind != null && other.Kind != null && !string.Equals(this.Kind, other.Kind))
                 return false;
 
             return string.Equals(this.Name, other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 31 + (this.ProjectId ?? -1).GetHashCode();
+                hash = hash * 31 + (this.Id ?? -1).GetHashCode();
+                hash = hash * 31 + (this.Kind ?? string.Empty).GetHashCode();
+                hash = hash * 31 + this.Name.GetHashCode();
+                return hash;
+            }
         }
 
         public override string ToString()
