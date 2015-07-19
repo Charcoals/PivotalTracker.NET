823981e450ae04e4e92b8ef4bb5fcbbba8d37c5e
Martin Hyldahl <martin@hyldahlnet.dk>
2015-07-19 21:31:57 +0200

    Updated Epic to match v5 API

--- PivotalTrackerDotNet/Domain/Epic.cs
+++ PivotalTrackerDotNet/Domain/Epic.cs
@@ -1,14 +1,45 @@
-﻿namespace PivotalTrackerDotNet.Domain
+﻿using System;
+using System.Collections.Generic;
+
+namespace PivotalTrackerDotNet.Domain
 {
     public class Epic
     {
-        public int id { get; set; }
-        public string kind { get; set; }
-        public string created_at { get; set; }
-        public string updated_at { get; set; }
-        public int project_id { get; set; }
-        public string name { get; set; }
-        public string url { get; set; }
-        public Label label { get; set; }
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+
+        public int LabelId { get; set; }
+        public Label Label { get; set; }
+
+        public string Description { get; set; }
+
+        public List<int> CommentIds { get; set; } // This field is excluded by default
+        public List<Comment> Comments { get; set; } // This field is excluded by default
+
+        public List<int> FollowerIds { get; set; } // This field is excluded by default
+        public List<Person> Follower { get; set; } // This field is excluded by default
+
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        // Unimplemented
+        public int AfterId { get; set; } // This field is excluded by default
+
+        // Unimplemented
+        public int BeforeId { get; set; } // This field is excluded by default
+
+        [Obsolete]
+        public float PastDoneStoryEstimates { get; set; } // This field is excluded by default
+
+        [Obsolete]
+        public float PastDoneStoriesCount { get; set; } // This field is excluded by default
+
+        [Obsolete]
+        public float PastDoneStoriesNoPointCount { get; set; } // This field is excluded by default
+
+        public string Url { get; set; }
+
+        public string Kind { get; set; }
     }
 }
