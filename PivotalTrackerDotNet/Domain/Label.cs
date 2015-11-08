912d44bb92365e770f1379375b3287d2ad020529
Tom Parker <palfrey@tevp.net>
2015-11-08 21:07:34 +0000

    Label equality is if the Names are the same

--- PivotalTrackerDotNet/Domain/Label.cs
+++ PivotalTrackerDotNet/Domain/Label.cs
@@ -10,8 +10,26 @@ namespace PivotalTrackerDotNet.Domain {
         public string Kind { get; set; }
         public int ID { get; set; }
 
-		public override string ToString() {
-			return String.Format("{0} - {1}", ID, Name);
-		}
+        public override string ToString() {
+            return String.Format("{0} - {1}", ID, Name);
+        }
+
+        public override bool Equals(Object obj) {
+            Label p = obj as Label;
+            if ((object)p == null)
+            {
+                return false;
+            }
+
+            return Name.Equals(p.Name);
+        }
+
+        public bool Equals(Label p) {
+            return Name.Equals(p.Name);
+        }
+
+        public override int GetHashCode() {
+            return Name.GetHashCode();
+        }
     }
 }
