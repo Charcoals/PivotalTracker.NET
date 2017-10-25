7d37599f37108d9216a65ef8acd3f4a1a30e657d
Martin Hyldahl <martin@hyldahlnet.dk>
2017-10-25 17:30:25 +0200

    Updated nuget pack

--- .nuget/nugetPack.cmd
+++ .nuget/nugetPack.cmd
@@ -1,6 +1,6 @@
 msbuild /t:Rebuild /p:Configuration=Release ..\PivotalTrackerDotNet.sln
-del /S /Q PivotalTrackerDotNet\lib\net40\*.*
-mkdir PivotalTrackerDotNet\lib\net40
-copy /Y ..\PivotalTrackerDotNet\bin\Release\PivotalTrackerDotNet.* PivotalTrackerDotNet\lib\net40
+del /S /Q PivotalTrackerDotNet\lib\net452\*.*
+mkdir PivotalTrackerDotNet\lib\net452
+copy /Y ..\PivotalTrackerDotNet\bin\Release\PivotalTrackerDotNet.* PivotalTrackerDotNet\lib\net452
 del PivotalTrackerDotNet.*.nupkg
 NuGet.exe pack PivotalTrackerDotNet/PivotalTrackerDotNet.nuspec
